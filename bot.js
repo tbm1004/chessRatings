a06999ed02c9214b729085b8eb9284cb2ff4ed04
Glitch (twitch-chatbot) <none>
2020-05-15 06:14:55 +0000

    🤗🎨 Checkpoint ./bot.js:16227990/1365

--- bot.js
+++ bot.js
@@ -40,28 +40,34 @@ function onMessageHandler (target, context, msg, self) {
 }
 
 function ranking (target, commandName) {
-    axios.get('https://api.chess.com/pub/player/hikaru/stats').then((response) => {
-    var x = JSON.stringify(response.data);
-    var spl = x.split("chess_blitz");
+    axios.get('https://api.chess.com/pub/player/hikaruuytiuytiuyt/stats').then((response) => {
+    try{
       
-    //regex for rating
-    var myRe = new RegExp('[0-9][0-9][0-9][0-9]?', 'g');
-    var myArray = myRe.exec(spl[1]);
-    var curRank = myArray[0];
+    }
+    var x = JSON.stringify(response.data);
 
-    //regex and formatting for wins/losses/ties
-    var statRe = new RegExp('"win".+?(?=}})', 'g');
-    var statArray = statRe.exec(spl[1]);
-    var g = statArray[0].replace('"', "");
-    var find = '"';
-    var repl = ','
-    var re = new RegExp(find, 'g');
-    var str = g.replace(re, '');
-    re = new RegExp(repl, 'g');
-    str = str.replace(re, ', ');
     
+        var spl = x.split("chess_blitz");
+
+        //regex for rating
+        var myRe = new RegExp('[0-9][0-9][0-9][0-9]?', 'g');
+        var myArray = myRe.exec(spl[1]);
+        var curRank = myArray[0];
+
+        //regex and formatting for wins/losses/ties
+        var statRe = new RegExp('"win".+?(?=}})', 'g');
+        var statArray = statRe.exec(spl[1]);
+        var g = statArray[0].replace('"', "");
+        var find = '"';
+        var repl = ','
+        var re = new RegExp(find, 'g');
+        var str = g.replace(re, '');
+        re = new RegExp(repl, 'g');
+        str = str.replace(re, ', ');
+
+
+        client.say(target, `Blitz stats for ${commandName}: Rating: ${curRank}, ${str}`);
     
-    client.say(target, `Stats for user ${commandName}: Rating: ${curRank}, ${str}`);
     })
 }
 
